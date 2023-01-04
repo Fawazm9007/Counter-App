61bd3adf7cf3444f46829bdff873c5774a76bb9b
Fawazm9007 <fawazmuhammed9007@gmail.com>
2023-01-04 19:21:09 +0530

    Counter App

---
+++ script.js
@@ -0,0 +1,49 @@
+let counter = document.querySelector('.counter-value')
+const decrease = document.querySelector('.decrease')
+const reset = document.querySelector('.reset')
+const increase = document.querySelector('.increase')
+const save = document.querySelector('.save')
+const load = document.querySelector('.load')
+const clear = document.querySelector('.clear')
+var counterValue = 0
+
+decrease.addEventListener('click',()=>{
+   counterValue--
+   counter.innerText = counterValue
+})
+increase.addEventListener('click',()=>{
+   
+   counterValue++
+   counter.innerText = counterValue
+ 
+})
+save.addEventListener('click' , () =>{
+      
+   localStorage.setItem('Value',counterValue)
+   console.log(counterValue);
+})
+reset.addEventListener('click' , () =>{
+   
+   counter.innerText = 0
+   counterValue=0
+})
+
+load.addEventListener('click' , () =>{
+      
+   let counterload = localStorage.getItem('Value')
+   counter.innerText = counterload
+   if(localStorage.getItem('Value')){
+      let counterload = localStorage.getItem('Value')
+   counter.innerText = counterload
+   }else{
+      counter.innerText = counterValue
+   }
+})
+clear.addEventListener('click' , () =>{
+   localStorage.removeItem('Value')
+   counter.innerText = 0
+})
+
+
+
+
